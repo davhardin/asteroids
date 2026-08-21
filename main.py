6e756a31c11308cae5b1f38e2502da367a3747dd
davhardin <dwhardin18@gmail.com>
2026-08-21 10:53:14 -0500

    1.0.1 create first game loop and limit FPS

--- main.py
+++ main.py
@@ -1,10 +1,28 @@
 import pygame
 from constants import SCREEN_WIDTH, SCREEN_HEIGHT
+from logger import log_state
 
 def main():
+    pygame.init()
     print(f"Starting Asteroids with pygame version: {pygame.version.ver}")
+
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
+
+    pygame.time.Clock()
+    dt = 0.0
+
+    while True:
+        log_state()
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return
+        screen.fill(color="black")
+        pygame.display.flip()
+        dt = clock.tick(60) / 1000
+        # print(dt)
+    
 
 
 if __name__ == "__main__":
