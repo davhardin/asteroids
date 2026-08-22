de0438b8d4399c140c7e653c0f3ac3dcad3f6752
davhardin <dwhardin18@gmail.com>
2026-08-22 04:37:44 -0500

    1.0.3 added player object

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import pygame
-from constants import SCREEN_WIDTH, SCREEN_HEIGHT
+from constants import PLAYER_RADIUS, SCREEN_WIDTH, SCREEN_HEIGHT
 from logger import log_state
+from player import Player
 
 def main():
     pygame.init()
@@ -13,16 +14,19 @@ def main():
     clock = pygame.time.Clock()
     dt = 0.0
 
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2, PLAYER_RADIUS)
+
     while True:
         log_state()
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         screen.fill(color="black")
+        player.draw(screen)
         pygame.display.flip()
         dt = clock.tick(60) / 1000
         # print(dt)
-    
+
 
 
 if __name__ == "__main__":
---
+++ player.py
@@ -0,0 +1,24 @@
+from typing import override
+
+import pygame
+
+from constants import LINE_WIDTH, PLAYER_RADIUS
+from circleshape import CircleShape
+
+class Player(CircleShape):
+    def __init__(self, x: float, y: float, radius: float) -> None:
+        super().__init__(x, y, radius)
+        self.radius = PLAYER_RADIUS
+        self.rotation: float = 0
+
+    def triangle(self) -> list[pygame.Vector2]:
+        forward = pygame.Vector2(0, 1).rotate(self.rotation)
+        right = pygame.Vector2(0, 1).rotate(self.rotation + 90) * self.radius / 1.5
+        a = self.position + forward * self.radius
+        b = self.position - forward * self.radius - right
+        c = self.position - forward * self.radius + right
+        return [a, b, c]
+
+    @override
+    def draw(self, screen: pygame.Surface) -> None:
+        pygame.draw.polygon(screen, "white", self.triangle(), LINE_WIDTH)
